015eb902c7a53f5fd9101f9c252704e412154d4b
subject026 <subject026@protonmail.com>
2022-07-30 13:38:14 +0100

    split header out to own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Admin from './routes/Admin';
 import Home from './routes/Home';
 
 function App() {
   return (
-    <>
-      <header className="bg-pink-100">
-        <div className="max-w-6xl m-auto p-4 flex justify-between items-center">
-          <h1 className="text-neutral-600 font-bold text-2xl">DAOptimate</h1>
-          <nav>
-            <Link to="/" className="px-4 text-neutral-600 hover:text-neutral-800">home</Link>
-            <Link to="/admin" className="px-4 text-neutral-600 hover:text-neutral-800">admin</Link>
-          </nav>
-        </div>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,13 @@
+import Header from '../components/Header';
+
 function Home() {
   return (
-    <main className="max-w-6xl m-auto p-4">
-      <p>Home</p>
-    </main>
+    <>
+      <Header />
+      <main className="max-w-6xl m-auto p-4">
+        <p>Home</p>
+      </main>
+    </>
   );
 }
 
